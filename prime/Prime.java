8dc469ff0c5aac8f999366034c08c93a15d897ac
froderik <fredrik@eldfluga.se>
2013-04-15 21:35:30 +0200

    green-5

--- prime/Prime.java
+++ prime/Prime.java
@@ -1,17 +1,21 @@
 package prime;
 
 public class Prime {
-  public static boolean isPrime(int number){
-    for(int i = 2; i <= sqrt(number); i++) {
-      int remainder = number % i;
-      if(remainder == 0) {
-        return false;
-      }
-    }
-    return true;
-  }
+	public static boolean isPrime(int number) {
+		for (int i = 2; i <= sqrt(number); i++) {
+			int remainder = number % i;
+			if (remainder == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-  private static double sqrt(int number){
-    return Math.sqrt(number);
-  }
+	private static double sqrt(int number) {
+		return Math.sqrt(number);
+	}
+
+	public static int[] factorsOf(int i) {
+		return new int[]{2,2};
+	}
 }
