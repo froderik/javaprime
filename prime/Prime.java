5cc065047f095494e2753f7c261b03a7f345220e
froderik <fredrik@eldfluga.se>
2013-04-15 22:04:42 +0200

    green-8

--- prime/Prime.java
+++ prime/Prime.java
@@ -19,7 +19,17 @@ public class Prime {
 		if(isPrime(number)){
 			return new int[]{number};
 		}
-		int factor = (int)Math.sqrt(number);
-		return new int[]{factor,factor};
+		for(int i = 2; i <= sqrt(number); i++){
+			if(isPrime(i) && number % i == 0){
+				int[] rest = factorsOf(number / i);
+				int[] total = new int[rest.length + 1];
+				total[0] = i;
+				for (int j = 0; j < rest.length; j++) {
+					total[j+1] = rest[j];
+				}
+				return total;
+			}
+		}
+		return new int[]{number};
 	}
 }
