3079640702ee9828fcd0c049088b5182c54044e7
froderik <fredrik@eldfluga.se>
2013-04-15 19:42:42 +0200

    refactor-4

--- prime/Prime.java
+++ prime/Prime.java
@@ -2,11 +2,16 @@ package prime;
 
 public class Prime {
   public static boolean isPrime(int number){
-    for(int i = 2; i < number; i++) {
-      if(number % i == 0) {
+    for(int i = 2; i <= sqrt(number); i++) {
+      int remainder = number % i;
+      if(remainder == 0) {
         return false;
       }
     }
     return true;
+  }
+
+  private static double sqrt(int number){
+    return Math.sqrt(number);
   }
 }
