c400f4e5e7a207e8514520560e33a94fa5c7b32a
froderik <fredrik@eldfluga.se>
2013-04-15 19:13:54 +0200

    green-3

--- prime/Prime.java
+++ prime/Prime.java
@@ -2,7 +2,7 @@ package prime;
 
 public class Prime {
   public static boolean isPrime(int number){
-    if( number == 4 ) return false;
+    if( number == 4 || number == 9 ) return false;
     return true;
   }
 }
