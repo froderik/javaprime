26c09c98a89eb19efe0cb8e604262b7dc164f7c7
froderik <fredrik@eldfluga.se>
2013-04-15 21:47:23 +0200

    refactor-6.5

--- prime/TestPrimeFactors.java
+++ prime/TestPrimeFactors.java
@@ -1,21 +1,36 @@
 package prime;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class TestPrimeFactors {
-
-	@Test
-	public void factorsOf4(){
-		int[] expected = new int[]{2,2};
-		int[] actual = Prime.factorsOf(4);
-		assertArrayEquals(expected,actual);
-	}
 	
-	@Test
-	public void factorsOf9(){
-		int[] expected = new int[]{3,3};
-		int[] actual = Prime.factorsOf(9);
-		assertArrayEquals(expected,actual);
+	private int number;
+	private int[] factors;
+	
+    public TestPrimeFactors(int number, int[] factors) {
+		this.number = number;
+		this.factors = factors;
 	}
+
+	@Parameters
+    public static Collection<Object[]> bunchOfPrimes(){
+        return Arrays.asList(new Object[][]{
+        		{4, new int[]{2,2}},
+        		{9, new int[]{3,3}}
+        });
+    }
+    
+    @Test
+    public void assertFactors(){
+    	assertArrayEquals(this.factors, Prime.factorsOf(this.number));
+    }
 }
